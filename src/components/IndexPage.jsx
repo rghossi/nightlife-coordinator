69408ecbb6807695952258b69ac04e2361ddfb9c
rghossi <rafael.ghossi@gmail.com>
2017-02-12 00:47:26 -0200

    added search box

--- src/components/IndexPage.jsx
+++ src/components/IndexPage.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { FormGroup, InputGroup, FormControl, Button } from 'react-bootstrap';
 
 export default class IndexPage extends React.Component {
-  render() {
-    return <div> Hello world! </div>
-  }
+	search(e) {
+		e.preventDefault();
+		//DO search using search api
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.search}>
+		    <FormGroup>
+		      <InputGroup>
+		      	<FormControl type="text" placeholder="Enter your location" />
+		        <InputGroup.Button>
+		          <Button type="submit">Search</Button>
+		        </InputGroup.Button>
+		      </InputGroup>
+		    </FormGroup>
+		    </form>
+		)
+	}
 }
