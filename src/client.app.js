26333d36cad48b803db631c1e11761e8488015d2
rghossi <rafael.ghossi@gmail.com>
2017-02-12 00:25:21 -0200

    fix missing css when using webpack dev middleware

--- src/client.app.js
+++ src/client.app.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './components/AppRoutes';
 
+if (process.env.NODE_ENV !== 'production')
+	require('style-loader!css-loader!./static/css/style.css');
+
 window.onload = () => {
   ReactDOM.render(<AppRoutes/>, document.getElementById('main'));
 };
